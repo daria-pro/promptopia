0bf409d6658ae6bbfa9309ad0eba6ba0ed440c6a
daria-pro <shaulkod@gmail.com>
2023-06-22 22:45:57 +0300

    add fetch function

--- app/page.jsx
+++ app/page.jsx
@@ -2,13 +2,29 @@
 import Feed from "@components/Feed";
 import { useEffect, useState } from "react";
 
+async function getData() {
+  const res = await fetch("/api/prompt");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
 const Home = () => {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("/api/prompt", { cache: "no-store" });
-      const data = await response.json();
-      setPosts(data);
+      // const response = await fetch("/api/prompt", { cache: "no-store" });
+      const response = await getData();
+      console.log(response);
+
+      setPosts(response);
     };
     fetchPosts();
   }, []);
