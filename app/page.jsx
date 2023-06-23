801846c4e651e32343d969b1af3ce529ef1a273b
daria-pro <shaulkod@gmail.com>
2023-06-23 08:37:29 +0300

    proper data fetching

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,21 @@
-"use client";
 import Feed from "@components/Feed";
-import { useEffect, useState } from "react";
 
-async function getData() {
-  const res = await fetch("/api/prompt");
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+export default async function () {
+  const response = await fetch(`${process.env.REQUEST_URL}/api/prompt`, {
+    cache: "no-store",
+  });
+  const data = await response.json();
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     // const response = await fetch("/api/prompt", { cache: "no-store" });
+  //     const response = await getData();
+  //     console.log(response);
 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
-
-const Home = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    const fetchPosts = async () => {
-      // const response = await fetch("/api/prompt", { cache: "no-store" });
-      const response = await getData();
-      console.log(response);
-
-      setPosts(response);
-    };
-    fetchPosts();
-  }, []);
+  //     setPosts(response);
+  //   };
+  //   fetchPosts();
+  // }, []);
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
@@ -38,9 +26,7 @@ const Home = () => {
         Promptopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
-      <Feed data={posts} />
+      <Feed data={data} />
     </section>
   );
-};
-
-export default Home;
+}
