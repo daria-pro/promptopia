787bd04f4730ab39d0ced632c3a601659b916ca7
daria-pro <shaulkod@gmail.com>
2023-07-12 15:56:04 +0300

    fix cache saving

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -4,11 +4,26 @@ import Prompt from "@models/prompt";
 export const GET = async (request) => {
   try {
     await connectToDB();
-    const prompts = await Prompt.find({}).populate("creator");
+    const prompts = await Prompt.find().populate({
+      path: "creator",
+    });
 
-    return new Response(JSON.stringify(prompts), {
+    const response = new Response(JSON.stringify(prompts), {
       status: 200,
     });
+
+    // Add a unique identifier to the URL to force a cache-busting reload
+    const url = new URL(req.url);
+    url.searchParams.set("t", Date.now());
+    response.headers.set(
+      "Cache-Control",
+      "no-cache, no-store, must-revalidate"
+    );
+    response.headers.set("Pragma", "no-cache");
+    response.headers.set("Expires", "0");
+    response.headers.set("Location", url.toString());
+
+    return response;
   } catch (error) {
     return new Response("Failed to fetch all prompts", {
       status: 500,
